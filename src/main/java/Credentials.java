f52b952a67ea6d95ae985141bb25f06e99efb747
f-olga <folga.ak@gmail.com>
2022-05-19 00:32:38 +0200

    Modify jenkins

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -1,5 +1,5 @@
 public class Credentials {
-    private final static String token = "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";
+    private final static String token = "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";
 
     protected static String getToken() {
         return token;
